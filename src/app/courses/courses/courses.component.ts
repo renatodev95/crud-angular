59558c6b03cd914858fc874ec9c54037c44a0d0d
Renato <renatodev95@gmail.com>
2021-12-22 17:39:02 -0300

    Banco de Dados H2 e Conectando o Angular na API Spring

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -15,7 +15,7 @@ import { ErrorDialogComponent } from './../../shared/components/error-dialog/err
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['_id','name', 'category'];
 
   // courseService: CoursesService;
 
